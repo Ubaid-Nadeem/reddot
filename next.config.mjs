32c4333876023473658a4883fc61a467525ccf24
muhammadYahya2007 <yahyanadeem2007@gmail.com>
2024-11-30 04:18:55 +0500

    changee eslink rules

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'radstore.pk',
-            
-          },
-        ],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "radstore.pk",
       },
+    ],
+  },
 };
 
 export default nextConfig;
